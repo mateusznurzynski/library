98467af11b8bc87396c9949e4a2291f48123328d
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-11-04 18:25:21 +0100

    Replace constructor functions with classes

--- main.js
+++ main.js
@@ -9,127 +9,131 @@ const STATUS_MESSAGE_FALSE = 'Status: Not read yet';
 newBookButton.addEventListener('click', toggleModal);
 modal.addEventListener('click', toggleModal);
 bookForm.addEventListener('click', (e) => {
-  e.stopPropagation();
+	e.stopPropagation();
 });
 submitButton.addEventListener('click', submitBookForm);
 
 let allBooks = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = Boolean(read);
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = Boolean(read);
+	}
+
+	toggleRead(e) {
+		this.read = this.read ? false : true;
+		e.target.classList.toggle('true');
+		if (this.read) {
+			e.target.textContent = STATUS_MESSAGE_TRUE;
+		} else {
+			e.target.textContent = STATUS_MESSAGE_FALSE;
+		}
+	}
 }
 
-Book.prototype.toggleRead = function (e) {
-  this.read = this.read ? false : true;
-  e.target.classList.toggle('true');
-  if (this.read) {
-    e.target.textContent = STATUS_MESSAGE_TRUE;
-  } else {
-    e.target.textContent = STATUS_MESSAGE_FALSE;
-  }
-};
-
 function addNewBook(title, author, pages, read) {
-  allBooks.push(new Book(title, author, pages, read));
-  refreshBooks();
+	allBooks.push(new Book(title, author, pages, read));
+	refreshBooks();
 }
 
 addNewBook('Krzyżacy', 'Henryk Sienkiewicz', 1234, true);
 
 function refreshBooks() {
-  books.textContent = '';
-  createCards();
+	books.textContent = '';
+	createCards();
 }
 
 function createCards() {
-  const bookCard = document.createElement('div');
-  const cardTable = document.createElement('table');
-  bookCard.appendChild(cardTable);
-  bookCard.classList.add('book-card');
+	const bookCard = document.createElement('div');
+	const cardTable = document.createElement('table');
+	bookCard.appendChild(cardTable);
+	bookCard.classList.add('book-card');
 
-  const read = document.createElement('div');
-  read.classList.add('read');
+	const read = document.createElement('div');
+	read.classList.add('read');
 
-  const removeButton = document.createElement('button');
-  removeButton.classList.add('remove-button');
-  removeButton.textContent = 'Remove book';
+	const removeButton = document.createElement('button');
+	removeButton.classList.add('remove-button');
+	removeButton.textContent = 'Remove book';
 
-  const cardRow = document.createElement('tr');
-  const rowTitle = document.createElement('th');
-  const rowContent = document.createElement('td');
+	const cardRow = document.createElement('tr');
+	const rowTitle = document.createElement('th');
+	const rowContent = document.createElement('td');
 
-  cardRow.appendChild(rowTitle);
-  cardRow.appendChild(rowContent);
+	cardRow.appendChild(rowTitle);
+	cardRow.appendChild(rowContent);
 
-  allBooks.forEach((book) => {
-    const newBookCard = bookCard.cloneNode(true);
-    const newTitle = cardRow.cloneNode(true);
-    const newAuthor = cardRow.cloneNode(true);
-    const newPages = cardRow.cloneNode(true);
-    const alreadyRead = read.cloneNode(true);
-    const button = removeButton.cloneNode(true);
+	allBooks.forEach((book) => {
+		const newBookCard = bookCard.cloneNode(true);
+		const newTitle = cardRow.cloneNode(true);
+		const newAuthor = cardRow.cloneNode(true);
+		const newPages = cardRow.cloneNode(true);
+		const alreadyRead = read.cloneNode(true);
+		const button = removeButton.cloneNode(true);
 
-    newBookCard.setAttribute('data-book-id', allBooks.indexOf(book));
+		newBookCard.setAttribute('data-book-id', allBooks.indexOf(book));
 
-    newTitle.firstChild.textContent = 'Title: ';
-    newTitle.lastChild.textContent = book.title ? `„${book.title}”` : '';
+		newTitle.firstChild.textContent = 'Title: ';
+		newTitle.lastChild.textContent = book.title ? `„${book.title}”` : '';
 
-    newAuthor.firstChild.textContent = 'Author: ';
-    newAuthor.lastChild.textContent = book.author
-      ? book.author
-      : 'Not specified';
+		newAuthor.firstChild.textContent = 'Author: ';
+		newAuthor.lastChild.textContent = book.author
+			? book.author
+			: 'Not specified';
 
-    newPages.firstChild.textContent = 'Pages: ';
-    newPages.lastChild.textContent = book.pages ? book.pages : 'Not specified';
+		newPages.firstChild.textContent = 'Pages: ';
+		newPages.lastChild.textContent = book.pages
+			? book.pages
+			: 'Not specified';
 
-    alreadyRead.textContent = book.read
-      ? STATUS_MESSAGE_TRUE
-      : STATUS_MESSAGE_FALSE;
-    if (book.read) {
-      alreadyRead.classList.add('true');
-    }
+		alreadyRead.textContent = book.read
+			? STATUS_MESSAGE_TRUE
+			: STATUS_MESSAGE_FALSE;
+		if (book.read) {
+			alreadyRead.classList.add('true');
+		}
 
-    alreadyRead.addEventListener('click', (e) => {
-      book.toggleRead.call(book, e);
-    });
+		alreadyRead.addEventListener('click', (e) => {
+			book.toggleRead.call(book, e);
+		});
 
-    button.addEventListener('click', removeBook);
+		button.addEventListener('click', removeBook);
 
-    newBookCard.firstChild.appendChild(newTitle);
-    newBookCard.firstChild.appendChild(newAuthor);
-    newBookCard.firstChild.appendChild(newPages);
-    newBookCard.appendChild(alreadyRead);
-    newBookCard.appendChild(button);
+		newBookCard.firstChild.appendChild(newTitle);
+		newBookCard.firstChild.appendChild(newAuthor);
+		newBookCard.firstChild.appendChild(newPages);
+		newBookCard.appendChild(alreadyRead);
+		newBookCard.appendChild(button);
 
-    books.appendChild(newBookCard);
-  });
+		books.appendChild(newBookCard);
+	});
 }
 
 function toggleModal(e) {
-  modal.classList.toggle('active');
+	modal.classList.toggle('active');
 }
 
 function submitBookForm(e) {
-  const newTitle = document.querySelector('#book-title').value;
-  const newAuthor = document.querySelector('#book-author').value;
-  const newPages = document.querySelector('#book-pages').value;
-  const newRead = document.querySelector('#book-read').checked;
+	const newTitle = document.querySelector('#book-title').value;
+	const newAuthor = document.querySelector('#book-author').value;
+	const newPages = document.querySelector('#book-pages').value;
+	const newRead = document.querySelector('#book-read').checked;
 
-  if (!newTitle) {
-    return;
-  }
+	if (!newTitle) {
+		return;
+	}
 
-  addNewBook(newTitle, newAuthor, newPages, newRead);
-  toggleModal();
-  bookForm.reset();
-  e.preventDefault();
+	addNewBook(newTitle, newAuthor, newPages, newRead);
+	toggleModal();
+	bookForm.reset();
+	e.preventDefault();
 }
 
 function removeBook(e) {
-  const bookId = e.target.parentNode.getAttribute('data-book-id');
-  allBooks.splice(bookId, 1);
-  refreshBooks();
+	const bookId = e.target.parentNode.getAttribute('data-book-id');
+	allBooks.splice(bookId, 1);
+	refreshBooks();
 }
